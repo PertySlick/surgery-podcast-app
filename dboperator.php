ae8a9c092feadf73c31f42be2a0f3211044af1dd
Brent Taylor <brent4994@hotmail.com>
2017-10-30 19:48:56 -0700

    Add files via upload
    Added database operator functions, all working but search feature

---
+++ dboperator.php
@@ -0,0 +1,219 @@
+<?php
+
+/*
+ * File Name: dboperator.php
+ * Author: Timothy Roush
+ * Date Created: 10/30/2017
+ * Assignment: Behind the Knife: Surgery Podcast database operations
+ * Description:  Model MVC Component - Handles Database Operations
+ */
+
+/**
+ * DbOperator represents an instance of a "model" component of the MVC style
+ * architecture.  This class handles any and all database interaction
+ * operations at the request of the controller component.  Class employs PDO
+ * SQL queries and statements to sanitize inputs as they are entered into the
+ * database.  Operator designed to always be passing either system determined
+ * integer values or CLASS/CLASS objects to and from methods where ever
+ * possible.  This operator will only work with the CLASS and CLASS classes.
+ *
+ * @author Timothy Roush
+ * @copyright 2017
+ * @version 1.0
+ * @see CLASS
+ * @see CLASS
+ */
+class DbOperator
+{
+
+
+// FIELDS - CONSTANTS AND OBJECTS
+
+
+    private $_conn;                 // Database Connection Object
+
+
+// CONSTRUCTOR
+
+
+    /**
+     * Creates an instance of a database interaction object.  Requires access
+     * to an externally stored credentials file for accessing the database.
+     * @throws PDOException if error encountered while establishing connection
+     */
+    public function __construct()
+    {
+        // Require Configuration File
+        // IMPORTANT: For this assignment store credentials in the following path:
+        //      home/username/secure/credentials_budgetapp.inc.php
+        require_once '../../../secure/credentials_surgerypodcast.inc.php';
+
+        // Establish Database Connection And Set Attributes
+        try {
+            $this->_conn = new PDO( DB_DSN, DB_USER, DB_PASSWORD );
+            $this->_conn->setAttribute( PDO::ATTR_PERSISTENT, true );
+            $this->_conn->setAttribute(
+                PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION
+            );
+        } catch ( PDOException $e ) {
+            die( "(!) Error - Connection Failed: " . $e->getMessage() );
+        }
+    }
+
+
+// METHODS - USER OPERATIONS
+/**
+ *Checks the most recent podcast/record found within the database.
+ *GROUPS DATA BY podcast id in a descending list
+ *LIMIT 1 - the most recent record to be pulled.
+ *
+ */
+    public function checkMostRecentUpload($id)
+    {
+        
+        $stmt = $this->_conn->prepare("SELECT podcast_id, MAX(:title) AS most_recent_podcast FROM podcasts
+                                  GROUP BY podcast_id ORDER BY podcast_id DESC LIMIT 1");
+        $stmt->bindParam(":pod_castid", $id);
+        $stmt-execute();
+    
+    
+    }
+    
+    /*
+     *This function searches the database and returns a list of podcast titles in order.
+     *
+     */
+     public function checkAllPodcasts($id)
+    {
+        
+        $stmt = $this->_conn->prepare("SELECT title AS list_of_titles FROM podcasts
+                                  ORDER BY title ASC");
+        $stmt->bindParam(":pod_castid", $id);
+        $stmt-execute();
+    
+    }
+    
+    public function checkAllPodcastTags($id)
+    {
+        
+        $stmt = $this->_conn->prepare("SELECT tag_name AS list_of_tags FROM podcasts_tags
+                                  ORDER BY tag_name ASC");
+        $stmt->bindParam(":pod_castid", $id);
+        $stmt-execute();
+    
+    }
+    
+    public function searchByKeyword($tag_name,$title,$description)
+    {
+        
+    }
+    
+    /**
+ * Checks the top 3 recent podcasts and displays them in descending order
+ * LIMIT 3 - The top 3 recent podcast rows
+ * GROUP BY podcast_id - the auto incremented number each time a podcast is added
+ */
+    public function checkTopThreePodcasts($id)
+    {
+        $stmt = $this->_conn->prepare("SELECT * FROM podcasts GROUP BY podcast_id ORDER BY podcast_id DESC LIMIT 3");
+        $stmt->bindParam(":pod_castid", $id);
+        $stmt-execute();
+    
+    
+    }
+    
+    public function addPodcastTag($tag_name)
+    {
+        $stmt = $this->_conn->prepare('INSERT INTO podcast_tags ' .
+                                      '(tag_name) ' .
+                                      'VALUES (:tag_name) ');
+        $stmt->bindParam(':tag_name', $tag_name, PDO::PARAM_STR);
+
+        try {
+            $stmt->execute();
+        } catch (PDOException $error) {
+            die ("(!) There was an error adding " . $tag_name . " to the database... " . $error);
+        }
+
+        return $this->_conn->lastInsertId();
+    }
+
+
+/**
+ *Checks to see if podcast exists within the database.
+ *@param $title, the title we are searching for in the db
+ */
+    public function checkPodcastExists($title)
+    {
+        $stmt = $this->_conn->prepare("SELECT COUNT(*) as count FROM podcasts WHERE title=:title");
+        $stmt->bindParam("title", $title);
+        $stmt-execute();
+    
+        $results = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if ($results['count'] > 0)
+        {
+            return true;
+            else return false;
+        }
+    
+}
+
+
+
+
+    /**
+     * Checks the supplied credential against the records stored in th users
+     * database.  If a record is found with a matching username, the data
+     * for that record is retrieved.  The password values are then compared. If
+     * they match, an array of user data is then returned for Controller use.
+     * @param $email String email used in login attempt
+     * @param $password String hashed value of password used in login attempt
+     * @return array of user data if validated, null otherwise
+     */
+    public function checkAdminCredentials($user, $password)
+    {
+        $stmt = $this->_conn->prepare("SELECT * FROM users WHERE username=:username");
+        $stmt->bindParam(":username", $user);
+        $stmt->execute();
+    
+        if ($stmt->rowCount() > 0) {
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+            if ($password === $result['password']) {
+                return array(
+                'username' => $result['username'],
+                'user_id' => $result['user_id'],
+                'email' => $result['email']
+                );
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Creates a new user record in the database with the supplied information.
+     *///userName,email,password
+    public function addPodcast($title, $description, $image,$author,$url,$duration,$publish_date)
+    {
+        $stmt = $this->_conn->prepare('INSERT INTO podcasts ' .
+                                      '(title, description, image, author, url, duration, publish_date) ' .
+                                      'VALUES (:title, :description, :image, :author, :url, :duration, :publish_date) ');
+        $stmt->bindParam(':title', $title, PDO::PARAM_STR);
+        $stmt->bindParam(':description', $description, PDO::PARAM_STR);
+        $stmt->bindParam(':image', ($image), PDO::PARAM_STR);
+        $stmt->bindParam(':author', $author, PDO::PARAM_STR);
+        $stmt->bindParam(':url', $url, PDO::PARAM_STR);
+        $stmt->bindParam(':duration', ($duration), PDO::PARAM_STR);
+        $stmt->bindParam(':publish_date', ($duration), PDO::PARAM_STR);
+
+
+        try {
+            $stmt->execute();
+        } catch (PDOException $error) {
+            die ("(!) There was an error adding " . $title . " to the database... " . $error);
+        }
+
+        return $this->_conn->lastInsertId();
+    }
+
+}
