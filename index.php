788f41b234bf5d4c8b97bcf195badcb98913fa33
Tim Roush <perty_slick@hotmail.com>
2017-11-04 15:21:04 -0700

    Podcasts By Topic - Modified index.php to implement topic logic

--- index.php
+++ index.php
@@ -28,14 +28,14 @@ define('CSS_PATH', '/css/');
 define('JS_PATH', '/js/');
 
 // Initiate fat-free, and Controller objects and set defaults
-$f3 = Base::instance();                                          // Instance of Fat Free object
-$f3->set('DEBUG', 3);                                   // Set Fat Free debug level
-$f3->set('INC', INCLUDES);                              // Set Includes File Path
+$f3 = Base::instance();                            // Instance of Fat Free object
+$f3->set('DEBUG', 3);                              // Set Fat Free debug level
+$f3->set('INC', INCLUDES);                         // Set Includes File Path
 $f3->set('IMG', $f3->get('BASE') . IMAGE_PATH);    // Set Image File Path
 $f3->set('CSS', $f3->get('BASE') . CSS_PATH);      // Set CSS File Path
 $f3->set('JS', $f3->get('BASE') . JS_PATH);        // Set JavaScript File Path
-$f3->set('fontAwesome', false);                         // Initialize FontAwesome Toggle
-$controller = new Controller();                                  // MVC Controller object
+$f3->set('fontAwesome', false);                    // Initialize FontAwesome Toggle
+$controller = new Controller();                    // MVC Controller object
 
 //Instantiate the ? class
 //   $DB = new DB();
@@ -56,9 +56,9 @@ $f3->route('GET /home', function($f3) use ($controller) {
     echo Template::instance()->render('view/home.html');
 });
 
-// Topic/category route
-// TODO: Route needs to be completed with functionality
-$f3->route('GET /topic', function($f3) use ($controller) {
+//
+$f3->route('GET /topic', function($f3, $params) use ($controller) {
+    $controller->topic($f3, $params);
     echo Template::instance()->render('view/topic.html');
 });
 
