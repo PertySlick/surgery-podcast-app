98b205c1bc7fb76af01870b9cbf719723e30c6b7
Marlene Leano <mleano@mail.greenriver.edu>
2017-11-07 17:27:25 -0600

    if description of clicked title is already visible, hide description when same title is clicked again

--- js/results.js
+++ js/results.js
@@ -14,22 +14,27 @@ var description = $('.pc-description');
 $('document').ready(function() {
     // Add click listener to podcast title dive
     $('.pc-title').on('click', function() {
-        // Hide all descriptions in case one is open
-        description.hide();
-        
-        // Set all titles overflow to hidden and wrap
-        // to nowrap in case a title is visible
-        titleItself.css('overflow', 'hidden');
-        titleItself.css('white-space', 'nowrap');
-        
-        // Show only the desc for the clicked title
-        $(this).next('.pc-description').slideDown("fast");
-        
-        // Toggle title overflow 
-        $(this).children('.new-pc-title').css('overflow', 'visible');
-        
-        // Toggle title wrap
-        $(this).children('.new-pc-title').css('white-space', 'normal');
+        //If description of clicked title is already visible, slide up
+        if ($(this).next('.pc-description').is(':visible')) {
+            $(this).next('.pc-description').slideUp("fast");
+        } else {   
+            // Hide all descriptions in case one is open
+            description.hide();
+            
+            // Set all titles overflow to hidden and wrap
+            // to nowrap in case a title is visible
+            titleItself.css('overflow', 'hidden');
+            titleItself.css('white-space', 'nowrap');
+            
+            // Show only the desc for the clicked title
+            $(this).next('.pc-description').slideDown("fast");
+            
+            // Toggle title overflow 
+            $(this).children('.new-pc-title').css('overflow', 'visible');
+            
+            // Toggle title wrap
+            $(this).children('.new-pc-title').css('white-space', 'normal');
+        }
  /*       
         // Toggle visibility of description
         description.slideToggle("fast");
