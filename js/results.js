f0dfe0313197171f7e829a519075fe0922cf0004
Marlene Leano <mleano@mail.greenriver.edu>
2017-11-10 17:54:05 -0600

    cleaned up and condensed code

--- js/results.js
+++ js/results.js
@@ -10,39 +10,6 @@
 var title = $('.new-pc-title');
 var description = $('.pc-description');
 
-/*
-$('document').ready(function() {
-    // Add click listener to podcast title dive
-    $('.pc-title').on('click', function() {
-        // If description of clicked title is already visible, slide up
-        // and hide overflow of title
-        if ($(this).next('.pc-description').is(':visible')) {
-            $(this).next('.pc-description').slideUp("fast");
-            title.css('overflow', 'hidden');
-            title.css('white-space', 'nowrap');
-        } else {   
-            // Hide all descriptions in case one is open
-            description.hide("fast");
-            
-            // Set all titles overflow to hidden and wrap
-            // to nowrap in case a title is visible
-            title.css('overflow', 'hidden');
-            title.css('white-space', 'nowrap');
-            
-            // Show only the desc for the clicked title
-            $(this).next('.pc-description').slideDown("fast");
-            
-            // Toggle title overflow 
-            $(this).children('.new-pc-title').css('overflow', 'visible');
-            
-            // Toggle title wrap
-            $(this).children('.new-pc-title').css('white-space', 'normal');
-        }
-    });
-});
-*/
-
-
 $('document').ready(function() {
     // Add click listener to podcast title dive
     $('.pc-title-div').on('click', function() {
@@ -50,24 +17,21 @@ $('document').ready(function() {
         // and hide overflow of title
         if ($(this).next('.pc-description').is(':visible')) {
             $(this).next('.pc-description').slideUp("fast");
-            title.css('overflow', 'hidden').css('white-space', 'nowrap').
-                css('font-size', '1em');
+            title.css('overflow', 'hidden').css('white-space', 'nowrap');
         } else {   
             // Hide all descriptions in case one is open
             description.hide("fast");
             
             // Set all titles overflow to hidden and wrap
             // to nowrap in case a title is visible
-            title.css('overflow', 'hidden');
-            title.css('white-space', 'nowrap');
+            title.css('overflow', 'hidden').css('white-space', 'nowrap');
             
             // Show only the desc for the clicked title
             $(this).next('.pc-description').slideDown("fast");
             
             // Toggle title overflow, wrap, and font-size (make larger)
             $(this).children('.new-pc-title').css('overflow', 'visible').
-                css('white-space', 'normal').
-                css('font-size', '1.1em');
+                css('white-space', 'normal');
         }
     });
 });
