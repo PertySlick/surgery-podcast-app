dafa08b7f907a37e12e4c5130991e7d2b6b36a43
Marlene Leano <mleano@mail.greenriver.edu>
2017-11-10 02:59:50 -0600

    removed iframe border

--- tmp/3l0ivo9qu90kk.2chqz1a5jnggs.php
+++ tmp/3l0ivo9qu90kk.2chqz1a5jnggs.php
@@ -11,9 +11,8 @@
 <h1 class="title">Surgery Legends</h1>
 <div class="subtitle">click to play</div>
 
-
 <?php foreach (($podcasts?:[]) as $podcast): ?>
-    <div class="new-pc-row pc-title">
+    <div class="new-pc-row pc-title" data-url="<?= ($podcast->getUrl()) ?>">
         <div class="new-pc-title"><?= ($podcast->getTitle()) ?></div>
     </div>
     <div class="new-pc-row pc-description">
@@ -25,6 +24,5 @@
     </div>
 <?php endforeach; ?>
 
-
-
 <?php echo $this->render($INC . 'footer.inc.html',NULL,get_defined_vars(),0); ?>
+    
