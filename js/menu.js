cec08535c1c66456997295f089a49f9baaba6a37
Marlene Leano <mleano@mail.greenriver.edu>
2017-11-26 01:36:06 -0600

    created js to toggle between showing and hiding keywords search bar and menu items

---
+++ js/menu.js
@@ -0,0 +1,9 @@
+/*
+    * File Name: menu.js
+    * Authors: Marlene Leano
+    * Date Created: 11/25/2017
+    * Assignment: Behind The Knife: The Surgery Podcast
+    * Description:  Toggle between showing and hiding menu items and
+    *               keyword(s) search bar.
+*/
+
