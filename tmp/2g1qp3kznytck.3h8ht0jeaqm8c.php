72dc3ce26d213708b904e91d0c8819d3bea50bcf
btaylor <btaylor17@mail.greenriver.edu>
2017-11-25 22:27:05 -0600

    added minimized form to homepage

--- tmp/2g1qp3kznytck.3h8ht0jeaqm8c.php
+++ tmp/2g1qp3kznytck.3h8ht0jeaqm8c.php
@@ -33,9 +33,23 @@
 
 <?php foreach (($podcasts?:[]) as $podcast): ?>
     <div class="recent-pc-row">
-        <div class="pc-title hide-title" data-url="<?= ($podcast->getUrl()) ?>"> 
-            <div class="pc-publish-date"><?= ($podcast->getPublishDate()) ?></div>
-            <span class="title-itself"><?= ($podcast->getTitle()) ?></span>
+        <div class="pc-title hide-title" data-url="<?= ($podcast->getUrl()) ?>">
+            <div class="title-itself pc-title-itself">
+                <div class="date-image-switch">
+                    <div class="pc-publish-date"><?= ($podcast->getPublishDateShort()) ?></div>
+                    <p class="true-title"><img src="<?= ($podcast->getImage()) ?>" class="pc-image"><?= ($podcast->getTitle()) ?></p>
+                </div>
+                
+                <div class="open-podcast-info">
+                    <p>Published Date: <br /><?= ($podcast->getPublishDateLong()) ?></p>
+                </div>
+                <div class="open-podcast-info">
+                    <i class="fa fa-download pc-download-button" aria-hidden="true"></i>
+                </div>
+                <div class="open-podcast-info" data-toggle="player">
+                    <i class="fa fa-play pc-play-button" aria-hidden="true"></i>
+                </div>
+            </div>
         </div>
         <div class="pc-description">
             <div class="pc-desc">
@@ -47,7 +61,7 @@
 " ?>
             </div>
         </div>
-    </div>        
+    </div>     
 <?php endforeach; ?>
 
 <!--Episodes by Topic-->
@@ -55,7 +69,7 @@
 
 <!--Search bar-->
 <div class="search-bar">
-    <input type="search" name="search-term" placeholder="Search for Topic">
+    <input type="search" id="topic-search-bar" name="search-term" placeholder="Search for Topic">
     <button type="submit">
         <i class="fa fa-search fa-2x" aria-hidden="true"></i>
     </button>  
@@ -74,8 +88,9 @@
     <?php endforeach; ?>
 </div>
 
+
 <!-- Begin MailChimp Signup Form -->
-<link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
+<link href="//cdn-images.mailchimp.com/embedcode/slim-10_7.css" rel="stylesheet" type="text/css">
 <style type="text/css">
 	#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
 	/* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
@@ -84,49 +99,14 @@
 <div id="mc_embed_signup">
 <form action="https://greenrivertech.us17.list-manage.com/subscribe/post?u=5f127826b33735e960540fc04&amp;id=61ad1d819a" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
     <div id="mc_embed_signup_scroll">
-	<h2>Subscribe to our mailing list</h2>
-<div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
-<div class="mc-field-group">
-    <!--Email Field-->
-	<label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
-</label>
-	<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
-</div>
-<div class="mc-field-group">
-    <!--First name field-->
-	<label for="mce-FNAME">First Name </label>
-	<input type="text" value="" name="FNAME" class="" id="mce-FNAME">
-</div>
-<div class="mc-field-group">
-    <!--Last name field-->
-	<label for="mce-LNAME">Last Name </label>
-	<input type="text" value="" name="LNAME" class="" id="mce-LNAME">
-</div>
-<div class="mc-field-group size1of2">
-    <!--Birthday Field-->
-	<label for="mce-BIRTHDAY-month">Birthday </label>
-	<div class="datefield">
-		<span class="subfield monthfield"><input class="birthday " type="text" pattern="[0-9]*" value="" placeholder="MM" size="2" maxlength="2" name="BIRTHDAY[month]" id="mce-BIRTHDAY-month"></span> / 
-		<span class="subfield dayfield"><input class="birthday " type="text" pattern="[0-9]*" value="" placeholder="DD" size="2" maxlength="2" name="BIRTHDAY[day]" id="mce-BIRTHDAY-day"></span> 
-		<span class="small-meta nowrap">( mm / dd )</span>
-	</div>
-</div><div class="mc-field-group input-group">
-    <strong>Email Format </strong>
-    <ul><li><input type="radio" value="html" name="EMAILTYPE" id="mce-EMAILTYPE-0"><label for="mce-EMAILTYPE-0">html</label></li>
-<li><input type="radio" value="text" name="EMAILTYPE" id="mce-EMAILTYPE-1"><label for="mce-EMAILTYPE-1">text</label></li>
-</ul>
-</div>
-	<div id="mce-responses" class="clear">
-		<div class="response" id="mce-error-response" style="display:none"></div>
-		<div class="response" id="mce-success-response" style="display:none"></div>
-	</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+	<label for="mce-EMAIL">Subscribe to our mailing list</label>
+	<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
     <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_5f127826b33735e960540fc04_61ad1d819a" tabindex="-1" value=""></div>
     <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
     </div>
 </form>
 </div>
-<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='BIRTHDAY';ftypes[3]='birthday';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
-<!--End mc_embed_signup-->
 
 
 <?php echo $this->render($INC . 'footer.inc.html',NULL,get_defined_vars(),0); ?>
