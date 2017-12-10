b6f2e218f40d9bad348572b3e6320e1ab6e8591c
Marlene Leano <mleano@mail.greenriver.edu>
2017-12-09 21:21:02 -0600

    created js for updating, adding, and deleting podcast hosts

---
+++ js/admin-about-us.js
@@ -0,0 +1,18 @@
+/*
+    * File Name: admin-about-us.js
+    * Authors: Marlene Leano
+    * Date Created: 12/09/2017
+    * Assignment: Behind The Knife: The Surgery Podcast
+    * Description:  Used for updating, deleting, and adding a
+    * new podcast host in the admin page
+*/
+
+$(document).ready(function() {
+    $('a.delete-host').on('click', confirmDeletion);
+});
+
+//Function that validates form inputs
+function confirmDeletion(event)
+{
+    return confirm('Continuing will remove the podcast host from the About Us page.');
+}
